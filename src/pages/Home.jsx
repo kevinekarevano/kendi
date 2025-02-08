ab799b71483e311c5e98f215a7784acb79a19174
kevinekarevano <kev1n.ek4revano@gmail.com>
2025-02-08 13:10:35 +0700

    thank you

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,12 @@ function Home() {
   const [currentTime, setCurrentTime] = useState(moment().format("HH:mm A"));
   const ref = useRef(null);
 
-  
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -27,9 +32,19 @@ function Home() {
 
   return (
     <div className="bg-[#FEF2E8]  h-full relative">
+      <div className="fixed flex justify-center items-center z-50 bg-opacity-90 w-full h-screen bg-black">
+        <div className="flex flex-col items-center">
+          <img className="" src="/image/logo_kendi.svg" alt="" />
+          <h1 className="text-4xl te font-bold text-yellow-600 my-4">Terima Kasih !</h1>
+          <p className="text-white  text-center  font-light px-2 md:w-1/2   ">
+            Kami mengucapkan terima kasih yang sebesar-besarnya atas partisipasi dan dukungan Anda dalam acara Business Day <span className="font-bold">Kendi</span> (Kedai N'Cang Yadi). Penjualan kami telah{" "}
+            <span className="font-bold">sukses</span> dilaksanakan dan kami sangat menghargai setiap pembelian yang Anda lakukan. Terima Kasih.
+          </p>
+        </div>
+      </div>
       <div className="w-full h-screen z-0 fixed pattern-dots pattern-zinc-300 pattern-bg-transparent pattern-opacity-60 pattern-size-4"></div>
 
-      <a target="_blank" href="https://wa.me/6281211545130?text=Halo%20saya%20ingin%20order%20di%20KENDI.">
+      {/* <a target="_blank" href="https://wa.me/6281211545130?text=Halo%20saya%20ingin%20order%20di%20KENDI.">
         <div className="fixed right-5 opacity-95  transition-all animate-bounce  bottom-[85px] border-2 border-black  rounded-tr-3xl rounded-l-2xl  rounded-br-none bg-green-300 p-2 z-50">
           <p className="font-semibold md:text-base text-xs">Kontak disini</p>
         </div>
@@ -37,7 +52,7 @@ function Home() {
         <div className="fixed shadow md:right-4 shadow-zinc-600 animate-bounce rounded-full w-14 p-3 z-50 right-1 bottom-5">
           <img className="w-8 h-8" src="image/wa_logo.png" alt="" />
         </div>
-      </a>
+      </a> */}
 
       <div className="md:hidden">
         <Marquee className="p-2" items={item} />
